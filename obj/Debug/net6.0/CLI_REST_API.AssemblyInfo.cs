564e9e3ace928fdf285480b52f2990d71223b69d
Challenger <Raja3061@hotmail.com>
2022-09-02 11:38:51 -0400

    Added Docker SQLserver image and init usersecrets

--- obj/Debug/net6.0/CLI_REST_API.AssemblyInfo.cs
+++ obj/Debug/net6.0/CLI_REST_API.AssemblyInfo.cs
@@ -10,6 +10,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("a7a81634-adde-4bef-bf95-54d650340603")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("CLI_REST_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
